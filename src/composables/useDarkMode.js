e3cd53bf3f9d1de0a0f70a58f14db0fc48920395
Chana <chana@channeldock.com>
2026-01-23 06:19:42 +0100

    Add dark mode support with theme variables and useDarkMode composable

---
+++ src/composables/useDarkMode.js
@@ -0,0 +1,17 @@
+import { useDark, useToggle } from '@vueuse/core'
+
+export function useDarkMode() {
+  const isDark = useDark({
+    selector: 'html',
+    attribute: 'class',
+    valueDark: 'dark',
+    valueLight: '',
+  })
+
+  const toggle = useToggle(isDark)
+
+  return {
+    isDark,
+    toggle,
+  }
+}
